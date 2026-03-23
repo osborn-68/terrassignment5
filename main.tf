7fba4ee347b137c97c7734fb43ad900f856ef52f
osborn-68 <tarun130p@gmail.com>
2026-03-23 12:55:22 +0530

    Increase volume size from 20 to 30

--- main.tf
+++ main.tf
@@ -102,7 +102,7 @@ resource "aws_instance" "apache_ec2" {
   EOF
 
   root_block_device {
-    volume_size           = 20
+    volume_size           = 30
     volume_type           = "gp3"
     delete_on_termination = true
     encrypted             = true
